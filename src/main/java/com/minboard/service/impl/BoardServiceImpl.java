d25cf05f94645bf3056510a1c4211e98b9beab28
younsikMin <minys94@daum.net>
2022-03-04 18:56:32 +0900

    BoardServiceImpl

--- src/main/java/com/minboard/service/impl/BoardServiceImpl.java
+++ src/main/java/com/minboard/service/impl/BoardServiceImpl.java
@@ -2,34 +2,78 @@ package com.minboard.service.impl;
 
 
 import com.minboard.dto.BoardDto;
-import com.minboard.repository.BoardRepository;
+import com.minboard.mapper.BoardMapper;
+import com.minboard.paging.PaginationDto;
 import com.minboard.service.BoardService;
 import com.minboard.vo.BoardVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService {
 
-    private final BoardRepository boardRepository;
+    private final BoardMapper boardMapper;
 
 
     @Override
     public int createBoard(BoardVo boardVo) {
 
-        int success = boardRepository.createBoard(boardVo);
+        int successCreate = boardMapper.createBoard(boardVo);
 
-        return success;
+        return successCreate;
     }
+
+
+
+    @Override
+    public BoardDto getDetailViewBoard(int id) {
+        return null;
+    }
+
+    @Override
+    public int deleteBoard(int boardId) {
+
+        int successDelete = boardMapper.deleteBoard(boardId);
+        return successDelete;
+    }
+
+    @Override
+    public int updateBoard(int boardId) {
+
+        int successUpdate = boardMapper.updateBoard(boardId);
+        return successUpdate;
+    }
+
 
     @Override
     public List<BoardDto> findAllByBoard() {
 
-        List<BoardDto> boardList = boardRepository.findAllByBoard();
+        List<BoardDto> boardList = boardMapper.findAllByBoard();
 
         return boardList;
+    }
+
+    @Override
+    public List<BoardDto> getBoardList(BoardVo boardVo) {
+        List<BoardDto> boardList = Collections.emptyList();
+        int boardTotalCount = boardMapper.geTotalBoardCount();
+
+        if(boardTotalCount > 0){
+            boardList = boardMapper.getBoardPagingList(boardVo);
+        }
+
+        return boardList;
+    }
+
+
+    @Override
+    public int geTotalBoardCount() {
+
+        int successCount = boardMapper.geTotalBoardCount();
+        return successCount;
     }
 }

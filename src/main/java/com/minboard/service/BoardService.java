c0ffeb45ff0827f44d3a1ed9268091f91eb5fb44
younsikMin <minys94@daum.net>
2022-03-04 18:56:18 +0900

    BoardService

--- src/main/java/com/minboard/service/BoardService.java
+++ src/main/java/com/minboard/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.minboard.service;
 
 import com.minboard.dto.BoardDto;
+import com.minboard.paging.PaginationDto;
 import com.minboard.vo.BoardVo;
 
 import java.util.List;
@@ -8,8 +9,19 @@ import java.util.List;
 
 public interface BoardService {
 
-    int createBoard(BoardVo boardVo);
+    List<BoardDto> findAllByBoard(); //전체 출력
 
-    List<BoardDto> findAllByBoard();
+    // 게시물 페이징
+    List<BoardDto> getBoardList(BoardVo boardVo);
+
+    int geTotalBoardCount(); // 총 게시물 수
+
+    BoardDto getDetailViewBoard(int id); // 상세보기
+
+    int createBoard(BoardVo boardVo); //게시물 생성
+
+    int deleteBoard(int id); // 게시물 삭제
+
+    int updateBoard(int id); // 게시물 수정
 
 }

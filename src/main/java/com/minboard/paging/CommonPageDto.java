bec18a755e668ea17726f89c6ba8f5b32919b1bb
youns <minys94@daum.net>
2022-03-07 02:28:25 +0900

    CommonPageDto

--- src/main/java/com/minboard/paging/CommonPageDto.java
+++ src/main/java/com/minboard/paging/CommonPageDto.java
@@ -1,5 +1,6 @@
 package com.minboard.paging;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,15 +15,15 @@ public class CommonPageDto extends PaginationDto{
     /** 페이징 정보 */
     private PaginationInfo paginationInfo;
 
-    /** 삭제 여부 */
-    private String deleteYn;
-
     /** 등록일 */
     private LocalDateTime createTime;
 
     /** 수정일 */
     private LocalDateTime updateTime;
 
-    /** 삭제일 */
-    private LocalDateTime deleteTime;
+    public CommonPageDto(PaginationInfo paginationInfo, LocalDateTime createTime, LocalDateTime updateTime) {
+        this.paginationInfo = paginationInfo;
+        this.createTime = createTime.now();
+        this.updateTime = updateTime.now();
+    }
 }

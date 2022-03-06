2aa1c2718e20d4256fcdb9c29ff9041fdef31f71
youns <minys94@daum.net>
2022-03-07 02:20:46 +0900

    BoardController

--- src/main/java/com/minboard/controller/BoardController.java
+++ src/main/java/com/minboard/controller/BoardController.java
@@ -19,45 +19,61 @@ public class BoardController {
 
     private final BoardService boardService;
 
-
-
+    /** 게시물 페이지 **/
     @GetMapping("/new")
-    public String formBoard(@ModelAttribute("boardDto") BoardDto boardDto){
-        return "html/board";
+    public String formBoard(@ModelAttribute("boardVo") BoardVo boardVo, Model model){
+        model.addAttribute("boardVo", BoardVo.builder().build());
+        return "html/boardNew";
     }
 
+    /** 게시물 생성하기 **/
     @PostMapping("/new")
-    public String createBoard(BoardVo boardVo, Model model){
+    public String createBoard(@ModelAttribute("boardVo") BoardVo boardVo, Model model){
         int successBoard = boardService.createBoard(boardVo);
         model.addAttribute("successBoard", successBoard);
-        return "html/board";
+        return "html/boardNew";
     }
 
+    /** 게시물 리스트 **/
     @GetMapping("/list")
-    public String getBoardPagingList(@ModelAttribute("boardVo") BoardVo boardVo, Model model){
-        List<BoardDto> boardList = boardService.getBoardList(boardVo);
+    public String getBoardPagingList(BoardDto boardDto, Model model){
+        List<BoardDto> boardList = boardService.getBoardList(boardDto);
         model.addAttribute("boardList", boardList);
         return "html/boardList";
     }
 
-    //상세보기
-    @GetMapping()
-    public String getDetailBoardView(){
-        return "html/board";
+    /** 게시물 상세페이지 **/
+    @GetMapping("/view/{id}")
+    public String getDetailBoardView(@PathVariable("id") int id, Model model){
+        BoardDto detailViewBoard = boardService.getDetailViewBoard(id);
+        model.addAttribute("detailViewBoard", detailViewBoard);
+        return "html/boardDetail";
     }
 
-    //수정
-    @PatchMapping()
-    public String updateBoard(){
-        return "html/board";
-    }
-    //삭제, delYn = "Y"
-    @PatchMapping()
-    public String deleteBoard(){
-        return "html/board";
+    /** 게시물 수정하기 **/
+    @PostMapping("/update")
+    public String updateBoard(@ModelAttribute("boardDto") BoardDto boardDto, Model model){
+        int updateBoard = boardService.updateBoard(boardDto);
+        model.addAttribute("updateBoard", updateBoard);
+        return "html/boardEdit";
     }
 
+    /** 게시물 수정페이지 **/
+    @GetMapping("/update/{id}")
+    public String getUpdateBoardView(@PathVariable("id") int id, Model model){
+        BoardDto boardDto = boardService.getDetailViewBoard(id);
+        model.addAttribute("boardDto", boardDto);
+        return "html/boardEdit";
+    }
+
+    /** 게시물 삭제하기 **/
+    @PostMapping("/delete")
+    public String deleteBoard(int id){
+        boardService.deleteBoard(id);
+        return "html/boardDetail";
+    }
 
 
+    }
 
-}
+

20877b6bd0adcf72a2538ed532385f4a999bcc31
younsikMin <minys94@daum.net>
2022-03-07 20:19:22 +0900

    PaginationInfo

--- src/main/java/com/minboard/paging/PaginationInfo.java
+++ src/main/java/com/minboard/paging/PaginationInfo.java
@@ -1,5 +1,6 @@
 package com.minboard.paging;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -36,14 +37,55 @@ public class PaginationInfo {
 
 
     public PaginationInfo(PaginationDto paginationDto) {
+        if (paginationDto.getCurrentPageNo() < 1) {
+            paginationDto.setCurrentPageNo(1);
+        }
+        if (paginationDto.getRecordsPerPage() < 1 || paginationDto.getRecordsPerPage() > 100) {
+            paginationDto.setRecordsPerPage(10);
+        }
+        if (paginationDto.getPageSize() < 5 || paginationDto.getPageSize() > 20) {
+            paginationDto.setPageSize(10);
+        }
         this.paginationDto = paginationDto;
-        this.totalRecordCount = totalRecordCount;
-        this.totalPageCount = totalPageCount;
-        this.firstPage = firstPage;
-        this.lastPage = lastPage;
-        this.firstRecordIndex = firstRecordIndex;
-        this.lastRecordIndex = lastRecordIndex;
-        this.hasPreviousPage = hasPreviousPage;
-        this.hasNextPage = hasNextPage;
     }
+
+    public void setTotalRecordCount(int totalRecordCount) {
+        this.totalRecordCount = totalRecordCount;
+        if (totalRecordCount > 0) {
+            calculation();
+        }
+    }
+
+    private void calculation() {
+
+        /* 전체 페이지 수 (현재 페이지 번호가 전체 페이지 수보다 크면 현재 페이지 번호에 전체 페이지 수를 저장) */
+        totalPageCount = ((totalRecordCount - 1) / paginationDto.getRecordsPerPage()) + 1;
+        if (paginationDto.getCurrentPageNo() > totalPageCount) {
+            paginationDto.setCurrentPageNo(totalPageCount);
+        }
+
+        /* 페이지 리스트의 첫 페이지 번호 */
+        firstPage = ((paginationDto.getCurrentPageNo() - 1) / paginationDto.getPageSize()) * paginationDto.getPageSize() + 1;
+
+        /* 페이지 리스트의 마지막 페이지 번호 (마지막 페이지가 전체 페이지 수보다 크면 마지막 페이지에 전체 페이지 수를 저장) */
+        lastPage = firstPage + paginationDto.getPageSize() - 1;
+        if (lastPage > totalPageCount) {
+            lastPage = totalPageCount;
+        }
+
+        /* SQL의 조건절에 사용되는 첫번째 RNUM */
+        firstRecordIndex = (paginationDto.getCurrentPageNo() - 1) * paginationDto.getRecordsPerPage();
+
+        /* SQL의 조건절에 사용되는 마지막 RNUM */
+        lastRecordIndex = paginationDto.getCurrentPageNo() * paginationDto.getRecordsPerPage();
+
+        /* 이전 페이지 존재 여부 */
+        hasPreviousPage = firstPage != 1;
+
+        /* 다음 페이지 존재 여부 */
+        hasNextPage = (lastPage * paginationDto.getRecordsPerPage()) < totalRecordCount;
+    }
+
+
+
 }

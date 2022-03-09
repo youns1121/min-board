56c8e4fb687f2b76b8175e0cec9a6e7a8ca52497
youns <minys94@daum.net>
2022-03-10 01:43:30 +0900

    PaginationInfo

--- src/main/java/com/minboard/paging/PaginationInfo.java
+++ src/main/java/com/minboard/paging/PaginationInfo.java
@@ -36,6 +36,51 @@ public class PaginationInfo {
     private boolean hasNextPage;
 
 
+
+
+    private int findTotalPageCount(){
+        /** 전체 페이지 수 (현재 페이지 번호가 전체 페이지 수보다 크면 현재 페이지 번호에 전체 페이지 수를 저장) **/
+        totalPageCount = ((totalRecordCount - 1) / paginationDto.getRecordsPerPage()) + 1;
+        if (paginationDto.getCurrentPageNo() > totalPageCount) {
+            paginationDto.setCurrentPageNo(totalPageCount);
+        }
+        return totalPageCount;
+    }
+
+    private int findPageListFirstNo(){
+        /** 페이지 리스트의 첫 페이지 번호 **/
+        return firstPage = ((paginationDto.getCurrentPageNo() - 1) / paginationDto.getPageSize()) * paginationDto.getPageSize() + 1;
+    }
+
+    private int findPageListLastNo(){
+        /** 페이지 리스트의 마지막 페이지 번호 (마지막 페이지가 전체 페이지 수보다 크면 마지막 페이지에 전체 페이지 수를 저장) **/
+        lastPage = findPageListFirstNo() + paginationDto.getPageSize() - 1;
+        if (lastPage > getTotalPageCount()) {
+            return lastPage = getTotalPageCount();
+        }
+        return lastPage;
+    }
+
+    private int findFirstRecordIndex(){
+        /** SQL의 조건절에 사용되는 첫번째 RNUM **/
+        return firstRecordIndex = (paginationDto.getCurrentPageNo() - 1) * paginationDto.getRecordsPerPage();
+    }
+
+    private int findLastRecordIndex(){
+        /** SQL의 조건절에 사용되는 첫번째 RNUM **/
+        return lastRecordIndex = paginationDto.getCurrentPageNo() * paginationDto.getRecordsPerPage();
+    }
+
+    private boolean findPreviousPageYn(){
+        return hasPreviousPage = findPageListFirstNo() != 1;
+    }
+
+    private boolean findNextPageYn(){
+        return hasNextPage = (findPageListLastNo() * paginationDto.getRecordsPerPage()) < totalRecordCount;
+    }
+
+/////////////////////////////
+
     public PaginationInfo(PaginationDto paginationDto) {
         if (paginationDto.getCurrentPageNo() < 1) {
             paginationDto.setCurrentPageNo(1);
@@ -52,13 +97,24 @@ public class PaginationInfo {
     public void setTotalRecordCount(int totalRecordCount) {
         this.totalRecordCount = totalRecordCount;
         if (totalRecordCount > 0) {
-            calculation();
+            newCalculation();
         }
+    }
+
+    private void newCalculation() {
+        totalPageCount = findTotalPageCount();
+        firstPage = findPageListFirstNo();
+        lastPage = findPageListLastNo();
+        firstRecordIndex = findFirstRecordIndex();
+        lastRecordIndex = findLastRecordIndex();
+        hasPreviousPage = findPreviousPageYn();
+        hasNextPage = findNextPageYn();
     }
 
     private void calculation() {
         /** 전체 페이지 수 (현재 페이지 번호가 전체 페이지 수보다 크면 현재 페이지 번호에 전체 페이지 수를 저장) **/
         totalPageCount = ((totalRecordCount - 1) / paginationDto.getRecordsPerPage()) + 1;
+
         if (paginationDto.getCurrentPageNo() > totalPageCount) {
             paginationDto.setCurrentPageNo(totalPageCount);
         }
